6504b754760da0d91e9d09f39a6202ba58101855
motimz <motimz@hotmail.com>
2014-01-08 11:39:37 +0200

    commit

--- ex3/BookBean.java
+++ ex3/BookBean.java
@@ -11,7 +11,7 @@ public class BookBean
 	private Date date;
 	private String description;
 	
-        public void setId(String str) { this.id = str; }
+    public void setId(String str) { this.id = str; }
 	public void setAuthor(String str) { this.author = str; }
 	public void setTitle(String str) { this.title = str; }
 	public void setGenre(String str) { this.genre = str; }
@@ -19,7 +19,7 @@ public class BookBean
 	public void setDate(Date date) { this.date = date; }
 	public void setDescription(String str) { this.description = str; }
 	
-        public String getId() { return this.id; }
+    public String getId() { return this.id; }
 	public String getAuthor() { return this.author; }
 	public String getTitle()  { return this.title; }
 	public String getGenre()  { return this.genre; }
